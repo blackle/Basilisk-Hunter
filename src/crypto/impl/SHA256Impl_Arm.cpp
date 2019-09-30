40062489b20fc51ea3407ef6cdda8ea42bff3810
blackle <isabelle@blackle-mori.com>
2019-09-29 21:59:39 -0400

    Only enable sha256 on armv8, and cpufeatures

--- src/crypto/impl/SHA256Impl_Arm.cpp
+++ src/crypto/impl/SHA256Impl_Arm.cpp
@@ -1,12 +1,26 @@
 #include "SHA256Impl_Arm.h"
 #include "bits_arm/sha256-armv8.h"
 
+#if __ARM_ARCH == 8
+#include "cpuinfo_arm.h"
+static const ArmFeatures features = GetArmInfo().features;
+#endif
+
 void SHA256Impl_Arm::calc_block(SHA256State* state, const SHA256Block* block) const
 {
+#if __ARM_ARCH == 8
 	sha256_armv8(state->data(), block->data(), 1);
+#else
+	(void) state;
+	(void) block;
+#endif
 }
 
 bool SHA256Impl_Arm::supported() const
 {
-	return true;
+#if __ARM_ARCH == 8
+	return features.sha2;
+#else
+	return false;
+#endif
 }
