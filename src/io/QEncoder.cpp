4a7c9903355b9175e90e3c45269e40c430bc4c88
blackle <isabelle@blackle-mori.com>
2019-10-07 17:51:38 -0400

    Make encoder use the stringstream better

--- src/io/QEncoder.cpp
+++ src/io/QEncoder.cpp
@@ -4,25 +4,29 @@
 #include <iomanip>
 #include <locale>
 
-struct QEncoding {
-	std::ostringstream os;
+class QEncoding {
+public:
 	void operator()(const uint8_t c) {
 		if (c > 127 || c == '=' || c == '?') {
-			os.str("");
-			os << std::hex
+			os << "="
+			   << std::hex
 			   << std::uppercase
 			   << std::setfill('0')
 			   << std::setw(2)
 			   << (int) c;
-			x += "=" + os.str();
 		} else {
-			x += c;
+			os << c;
 		}
 	}
-	std::string x;
+
+	std::string str() const {
+		return os.str();
+	}
+private:
+	std::ostringstream os;
 };
 
 std::string QEncoder::encode_utf8(const std::string& str) {
 	QEncoding encoding = std::for_each(str.begin(), str.end(), QEncoding());
-	return "=?utf-8?Q?" + encoding.x + "?=";
+	return "=?utf-8?Q?" + encoding.str() + "?=";
 }
