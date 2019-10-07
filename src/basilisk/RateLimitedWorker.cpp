ef38b1effa4ffee51a1b198486d76e082be4bbe4
blackle <isabelle@blackle-mori.com>
2019-10-07 00:47:30 -0400

    Get rid of FUDGE_FACTOR

--- src/basilisk/RateLimitedWorker.cpp
+++ src/basilisk/RateLimitedWorker.cpp
@@ -7,7 +7,6 @@
 namespace chrono = std::chrono;
 
 constexpr int MICROSECONDS_IN_SECOND = chrono::duration_cast<chrono::microseconds>(chrono::seconds(1)).count();
-constexpr float FUDGE_FACTOR = 0.9;
 
 RateLimitedWorker::RateLimitedWorker(SharedChallenge* winner, const Configuration* config, float limit)
 	: super(winner, config)
@@ -20,12 +19,15 @@ RateLimitedWorker::~RateLimitedWorker()
 
 void RateLimitedWorker::do_batch()
 {
-	m_speedometer.start();
+	if (m_speedometer.seconds() > 1) {
+		m_speedometer.start();
+	}
+
 	super::do_batch();
 
 	float mh = m_speedometer.million_hashes();
 	float seconds = m_speedometer.seconds();
-	float seconds_to_wait = std::max(mh/m_limit - seconds/FUDGE_FACTOR, 0.f);
+	float seconds_to_wait = std::max(mh/m_limit - seconds, 0.f);
 	uint64_t microseconds_to_wait = seconds_to_wait * MICROSECONDS_IN_SECOND;
 
 	std::this_thread::sleep_for(chrono::microseconds(microseconds_to_wait));
